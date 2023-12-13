d74625f8dab5467e7be13623e3d914ed8d15e3bf
Han JiHO AH NoteBook <h2j970313@gmail.com>
2023-12-14 00:11:22 +0900

    level1 없는 수 더하기 정답 추가 commit

--- src/main/java/level1/없는수더하기/Solution.java
+++ src/main/java/level1/없는수더하기/Solution.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Solution {
 
-    public int solution(int[] numbers) {
+    public int solution1(int[] numbers) {
         int answer = 0;
         int[] num = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         List<Integer> list = new ArrayList<>();
@@ -14,6 +14,9 @@ public class Solution {
         for (int n : numbers) list.remove(Integer.valueOf(n));
         for (int n : list) answer += n;
         return answer;
+    }
+    public int solution2(int[] numbers) {
+        return 45 - Arrays.stream(numbers).sum();
     }
 
 }
--- src/test/java/level1/없는수더하기/SolutionTest.java
+++ src/test/java/level1/없는수더하기/SolutionTest.java
@@ -14,15 +14,21 @@ class SolutionTest {
     void solution1() {
         Solution solution = new Solution();
         int[] numbers = {1, 2, 3, 4, 6, 7, 8, 0};
-        int answer = solution.solution(numbers);
-        Assertions.assertThat(answer).isEqualTo(14);
+        int expected = 14;
+        int answer1 = solution.solution1(numbers);
+        int answer2 = solution.solution2(numbers);
+        Assertions.assertThat(answer1).isEqualTo(expected);
+        Assertions.assertThat(answer2).isEqualTo(expected);
     }
     @Test
     void solution2() {
         Solution solution = new Solution();
         int[] numbers = {5, 8, 4, 0, 6, 7, 9};
-        int answer = solution.solution(numbers);
-        Assertions.assertThat(answer).isEqualTo(6);
+        int expected = 6;
+        int answer1 = solution.solution1(numbers);
+        int answer2 = solution.solution2(numbers);
+        Assertions.assertThat(answer1).isEqualTo(expected);
+        Assertions.assertThat(answer2).isEqualTo(expected);
     }
 
 }
