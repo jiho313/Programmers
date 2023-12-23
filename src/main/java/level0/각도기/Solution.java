a6bded0581b5f04b7829c3e2ba6aaf04dee51bcc
Han JiHo <h2j970313@gmail.com>
2023-12-23 23:46:03 +0900

    level0 각도기 commit

---
+++ src/main/java/level0/각도기/Solution.java
@@ -0,0 +1,23 @@
+package level0.각도기;
+
+public class Solution {
+
+    public int solution1(int angle) {
+        if (angle < 90) return 1;
+        if (angle == 90) return 2;
+        if (angle < 180) return 3;
+        return 4;
+    }
+
+    public int solution2(int angle) {
+        if (angle > 0 && angle < 90) {
+            return 1;
+        } else if (angle == 90) {
+            return 2;
+        } else if (angle > 90 && angle < 180) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
---
+++ src/test/java/level0/각도기/SolutionTest.java
@@ -0,0 +1,37 @@
+package level0.각도기;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void solution1() {
+        int angle = 70;
+        Solution solution = new Solution();
+        int answer1 = solution.solution1(angle);
+        int answer2 = solution.solution2(angle);
+        assertThat(answer1).isEqualTo(1);
+        assertThat(answer2).isEqualTo(1);
+    }
+    @Test
+    void solution2() {
+        int angle = 91;
+        Solution solution = new Solution();
+        int answer1 = solution.solution1(angle);
+        int answer2 = solution.solution2(angle);
+        assertThat(answer1).isEqualTo(3);
+        assertThat(answer2).isEqualTo(3);
+    }
+    @Test
+    void solution3() {
+        int angle = 180;
+        Solution solution = new Solution();
+        int answer1 = solution.solution1(angle);
+        int answer2 = solution.solution2(angle);
+        assertThat(answer1).isEqualTo(4);
+        assertThat(answer2).isEqualTo(4);
+    }
+
+}
