127300862e0a3c3616a1b9358ba1f197988b4c64
Han JiHo <h2j970313@gmail.com>
2023-12-25 18:50:32 +0900

    level0 문자열로 변환 commit

---
+++ src/main/java/level0/문자열로변환/Solution.java
@@ -0,0 +1,13 @@
+package level0.문자열로변환;
+
+public class Solution {
+
+    public String solution1(int n) {
+        return String.valueOf(n);
+    }
+
+    public String solution2(int n) {
+        return Integer.toString(n);
+    }
+
+}
--- src/test/java/level0/문자열로변환/SolutionTest.java
+++ src/test/java/level0/문자열로변환/SolutionTest.java
@@ -1,4 +1,4 @@
-package level0.약수구하기;
+package level0.문자열로변환;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,23 +8,23 @@ class SolutionTest {
 
     @Test
     void solution1() {
-        int n = 24;
+        int n = 123;
         Solution solution = new Solution();
-        int[] answer1 = solution.solution1(n);
-        int[] answer2 = solution.solution2(n);
-        int[] expected = {1, 2, 3, 4, 6, 8, 12, 24};
+        String expected = "123";
+        String answer1 = solution.solution1(n);
+        String answer2 = solution.solution2(n);
+        assertThat(answer1).isEqualTo(expected);
+        assertThat(answer2).isEqualTo(expected);
+    }
+    @Test
+    void solution2() {
+        int n = 2573;
+        Solution solution = new Solution();
+        String expected = "2573";
+        String answer1 = solution.solution1(n);
+        String answer2 = solution.solution2(n);
         assertThat(answer1).isEqualTo(expected);
         assertThat(answer2).isEqualTo(expected);
     }
 
-    @Test
-    void solution2() {
-        int n = 29;
-        Solution solution = new Solution();
-        int[] answer1 = solution.solution1(n);
-        int[] answer2 = solution.solution2(n);
-        int[] expected = {1, 29};
-        assertThat(answer1).isEqualTo(expected);
-        assertThat(answer2).isEqualTo(expected);
-    }
 }
