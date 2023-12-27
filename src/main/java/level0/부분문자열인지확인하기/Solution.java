22b13e0046dad70d19bc3667344eddcf3d39d6af
Han JiHo <h2j970313@gmail.com>
2023-12-27 23:53:13 +0900

    level0 부분 문자열인지 확인하기 commit

---
+++ src/main/java/level0/부분문자열인지확인하기/Solution.java
@@ -0,0 +1,8 @@
+package level0.부분문자열인지확인하기;
+
+public class Solution {
+
+    public int solution(String myString, String target) {
+        return myString.contains(target) ? 1 : 0;
+    }
+}
---
+++ src/test/java/level0/부분문자열인지확인하기/SolutionTest.java
@@ -0,0 +1,27 @@
+package level0.부분문자열인지확인하기;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+
+    @Test
+    void solution1() {
+        String myString = "banana";
+        String target = "ana";
+        Solution solution = new Solution();
+        int answer = solution.solution(myString, target);
+        assertThat(answer).isEqualTo(1);
+    }
+
+    @Test
+    void solution2() {
+        String myString = "banana";
+        String target = "wxyz";
+        Solution solution = new Solution();
+        int answer = solution.solution(myString, target);
+        assertThat(answer).isEqualTo(0);
+    }
+
+}
