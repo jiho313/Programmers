b1d7be340a4eb62877a29f7e51d5174c2a3d83ea
Han JiHo <h2j970313@gmail.com>
2024-01-28 23:23:09 +0900

    level0 배열 두배 만들기 commit

--- src/test/java/level0/배열두배만들기/SolutionTest.java
+++ src/test/java/level0/배열두배만들기/SolutionTest.java
@@ -1,10 +1,8 @@
 package level0.배열두배만들기;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
